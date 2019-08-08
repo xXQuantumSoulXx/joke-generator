7af8ff9ea410a7975b1eac76ce2dd511a98efa7b
xXQuantumSoulXx <42977009+xXQuantumSoulXx@users.noreply.github.com>
2019-08-08 21:07:10 +0100

    Initial Commit

---
+++ resources/app/index.js
@@ -0,0 +1,33 @@
+const jokes = require('./jokes.json')
+const {app, BrowserWindow} = require('electron')
+const num = require('./num.js')
+
+let win;
+
+console.log(num.num)
+
+function createWindow() {
+    win = new BrowserWindow({
+        height: 800,
+        width: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+    win.loadFile('./html/index.html')
+
+    win.on('closed', () => {
+        win = null;
+        return process.exit(0)
+    })
+}
+
+app.on('ready', () => {
+    createWindow()
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
